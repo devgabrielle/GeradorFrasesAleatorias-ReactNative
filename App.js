8a762c678ed3fea14ae56fe490187dfd681eb33d
Gabrielle Soares <55507831+devgabrielle@users.noreply.github.com>
2021-10-19 23:25:55 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import BiscoitoDaSorte from './src/home';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      < BiscoitoDaSorte />
+    </View>
+  );
+};
